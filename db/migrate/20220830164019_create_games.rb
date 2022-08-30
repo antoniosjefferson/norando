8e053af1d0740c86b08d7ec3de35d6369027695a
antoniosjefferson <itsmehtone@gmail.com>
2022-08-30 09:43:21 -0700

    creates game, user, and post model. deleted migration files with mistakes before migrating

---
+++ db/migrate/20220830164019_create_games.rb
@@ -0,0 +1,11 @@
+class CreateGames < ActiveRecord::Migration[7.0]
+  def change
+    create_table :games do |t|
+      t.string :title
+      t.string :release_date
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_164131) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "title"
+    t.string "release_date"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
