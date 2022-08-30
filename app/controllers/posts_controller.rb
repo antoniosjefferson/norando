fb88c42abced1dc9f49a814a38e248ed24ee06f1
antoniosjefferson <itsmehtone@gmail.com>
2022-08-30 12:08:08 -0700

    adds index and show routes for games and posts, adds create route for posts, adds methods to game and post controller, adds requests.http file with ability to create new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,23 @@
 class PostsController < ApplicationController
+  def index
+    posts = Post.all
+    render json: (posts)
+  end
+
+  def show
+    post = Post.find(params[:id])
+    render json: (post)
+  end
+
+  def create
+    post = Post.new(
+      title: params[:title],
+      body: params[:body],
+    )
+    if post.save
+      render json: post
+    else
+      render json: { errors: post.errors.full_messages }, status: 404
+    end
+  end
 end
