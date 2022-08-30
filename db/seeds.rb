3fed9fe12d5f62dad07b036b8de96256b230b1c2
antoniosjefferson <itsmehtone@gmail.com>
2022-08-30 10:38:01 -0700

    adds data to seeds.rb and runs seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+game = Game.create!(title: "Apex Legends", release_date: "02.04.2019", description: "Apex Legends is a free-to-play hero shooter game developed by Respawn Entertainment and published by Electronic Arts.")
+
+game = Game.create!(title: "Fortnite", release_date: "07.25.2017", description: "Fortnite is a survival game where 100 players fight against each other in player versus player combat to be the last one standing.")
+
+game = Game.create!(title: "Valorant", release_date: "06.02.2020", description: "Valorant is a team-based first-person hero shooter set in the near future.")
