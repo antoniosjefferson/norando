7483c03379d9fc336fe3765e231b908ac5d1939e
antoniosjefferson <itsmehtone@gmail.com>
2022-08-31 09:51:26 -0700

    installs bcrypt gem, adds all approved routes, adds sign up validations to user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,28 @@
 Rails.application.routes.draw do
+  # Show all games
   get "/games" => "games#index"
+
+  # Show specific game
   get "/games/:id" => "games#show"
+
+  # Show all posts
   get "/posts" => "posts#index"
-  get "/posts/:id" => "posts#show"
+
+  # Create post
   post "/posts" => "posts#create"
+
+  # Edit post
   patch "/posts/:id" => "posts#update"
+
+  # Sign up
+  post "/users" => "users#create"
+
+  # Sign in
+  post "/sessions" => "sessions#create"
+
+  # Destroy post
+  delete "/posts/:id" => "posts#destroy"
+
+  # Show specific post
+  # get "/posts/:id" => "posts#show"
 end
