f1b1aa9e0aa7e74b5b200257641b814350f720e4
antoniosjefferson <itsmehtone@gmail.com>
2022-09-01 12:05:21 -0700

    adds associations to game, post, and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :games
+  has_many :posts # can you do 'has_many :games, :posts'?
   has_secure_password
   validates :email, presence: true, uniqueness: true
 end
